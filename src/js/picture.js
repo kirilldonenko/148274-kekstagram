c705ee10b9398bf6036e983d53035349d1f1e34e
kirilldonenko <kirill.y.donenko@yandex.ua>
2016-11-26 21:40:41 +0200

    Picture.prototype.create

--- src/js/picture.js
+++ src/js/picture.js
@@ -2,7 +2,21 @@
 
 // функция отрисовки одной маленькой фотографии
 
-var getPictureMini = function(pic) {
+var gallery = require('./gallery');
+var Picture = function(pic, num) {
+  this.data = pic;
+  this.num = num;
+  this.element = this.create(pic);
+  this.onClick = this.onClick.bind(this);
+  this.element.addEventListener('click', this.onClick);
+};
+Picture.prototype.onClick = function(evt) {
+  evt.preventDefault();
+  if (evt.currentTarget.classList.contains('picture')) {
+    gallery.show(this.num);
+  }
+};
+Picture.prototype.create = function(pic) {
   var template = document.querySelector('#picture-template');
   var templateContainer = 'content' in template ? template.content : template;
   var PHOTO_LOAD_TIMEOUT = 10000;
@@ -14,13 +28,13 @@ var getPictureMini = function(pic) {
   var image = pictureMini.getElementsByTagName('img')[0];
   pictureMini.querySelector('.picture-comments').textContent = pic.comments;
   pictureMini.querySelector('.picture-likes').textContent = pic.likes;
-  photo.addEventListener('load', function handlerLoadPhoto() {
+  photo.addEventListener('load', function() {
     clearTimeout(photoTimeout);
     image.src = pic.url;
     image.width = IMAGE_WIDTH;
     image.leight = IMAGE_LEIGHT;
   });
-  photo.addEventListener('error', function handlerError() {
+  photo.addEventListener('error', function() {
     pictureMini.classList.add('picture-load-failure');
   });
   photo.src = pic.url;
@@ -29,22 +43,9 @@ var getPictureMini = function(pic) {
   }, PHOTO_LOAD_TIMEOUT);
   return pictureMini;
 };
-var gallery = require('./gallery');
-var Picture = function(pic, num) {
-  this.data = pic;
-  this.element = getPictureMini(pic);
-  this.onClick = this.onClick.bind(this);
-  this.element.addEventListener('click', this.onClick);
-  Picture.prototype.onClick = function(evt) {
-    evt.preventDefault();
-    if (evt.currentTarget.classList.contains('picture')) {
-      gallery.show(num);
-    }
-  };
-  Picture.prototype.remove = function() {
-    this.element.removeEventListener('click', this.onClick);
+Picture.prototype.remove = function() {
+  this.element.removeEventListener('click', this.onClick);
     //this.element.removeEventListener('load', handlerLoadPhoto);
     //this.element.removeEventListener('error', handlerError);
-  };
 };
 module.exports = Picture;
